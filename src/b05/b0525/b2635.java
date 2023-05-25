5726165dccf33b2c386634a8335a770e3a30601f
baggyeonghyeon <rudgus4620@gmail.com>
2023-05-25 14:44:39 +0900

    5월25일백준 문제풀이 완료

--- src/b05/b0525/b2635.java
+++ src/b05/b0525/b2635.java
@@ -4,19 +4,44 @@ import javax.imageio.IIOException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class b2635 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 양의 정수 ->
-        // 두번째 수는 양의 정수중 하나 선택
-        // 세번째부터 모든 수는 앞의 수에서 앞의  수를 빼서 만든거
-        // 3 -> 1-2
-        // 음만들어지면 음 버리고 그만
-        // 두번째수를 찾아서 그걸로 만든 수들과 개수를 구해라!
-
         int N = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for(int i =N; i>=N/2; i--) {
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(N);
+
+            int temp = N;
+            int temp2 = i;
+
+            while (true) {
+                if(temp<0) break;
+                int temp3 = temp;
+
+                if(temp2 >= 0) list.add(temp2);
+
+                temp = temp2;
+                temp2 = temp3 - temp;
+            }
+            if(count<list.size()) {
+                count = list.size();
+                result = list;
+            }
+        }
+        System.out.println(count);
+        for(Integer c: result) {
+            System.out.print(c+" ");
+        }
+
+
 
 
         br.close();
--- src/b05/b0525/b2872.java
+++ src/b05/b0525/b2872.java
@@ -3,10 +3,35 @@ package b05.b0525;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
 
 public class b2872 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for(int i =0; i<N; i++) {
+            arrayList.add(Integer.parseInt(br.readLine()));
+        }
+
+        int result = 0;
+        Collections.reverse(arrayList);
+
+        int target = N;
+
+        for(int i =0; i<N; i++) {
+            if(arrayList.get(i) == target) {
+                target--;
+            }else{
+                result++;
+            }
+        }
+
+        System.out.println(result);
 
         br.close();
     }
