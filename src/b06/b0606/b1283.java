9bc97157ae09e2cfac594c0d0cfeb991c58f377a
baggyeonghyeon <rudgus4620@gmail.com>
2023-06-06 21:32:12 +0900

    6월 6일 일백준 문제풀이 완료!

---
+++ src/b06/b0606/b1283.java
@@ -0,0 +1,15 @@
+package b06.b0606;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class b1283 {
+    static HashMap<Character, Integer> hashMap = new HashMap<>();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
---
+++ src/b06/b0606/b19949.java
@@ -0,0 +1,4 @@
+package b06.b0606;
+
+public class b19949 {
+}
